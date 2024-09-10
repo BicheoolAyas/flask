c1b578316c8ed3229db2742ee6b678eadb7afd2b
BicheoolAyas <bicheool.ayas@gmail.com>
2024-09-10 22:43:09 +0700

    Add new features. Add post, read post, error404

--- flask_khasan/flask_khasan/main.py
+++ flask_khasan/flask_khasan/main.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-from flask import Flask, render_template
+from flask import Flask, render_template, request, abort, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 # create the extension
@@ -10,6 +10,7 @@ app = Flask(__name__)
 
 # Configure the postgresql database
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:qwerty@127.0.0.1/news'
+app.config['SQLALCHEMY_ECHO'] = True # logs db
 
 # initialize the app with the extension
 db.init_app(app)
@@ -42,6 +43,13 @@ class Post(db.Model):
     def __repr__(self):
         return self.title
 
+# Forms
+from wtforms import Form, StringField, TextAreaField, SelectField
+
+class PostForm(Form):
+    title = StringField('Заголовок статьи')
+    content = TextAreaField('Текст статьи', render_kw={'rows': 15})
+    category = SelectField('Категория:', choices=[])
 
 
 @app.route('/')
@@ -66,6 +74,61 @@ def category_list(id: int):
                            categories=categories,
                            posts=posts,
                            current=current)
+
+@app.route('/post/<int:id>')
+def post_detail(id: int):
+    """Статья на отдельной странице"""
+    post = Post.query.filter(Post.id == id).first() # более современный вариант
+    # post = Post.query.get(id) # более старый вариант
+    # post = db.session.get(Post, id) # Или даже так
+    # post = db.session.execute(db.select(Post).filter_by(id=id)).scalar()
+
+    return render_template('news/post_detail.html', post=post)
+
+@app.route('/search/', methods=['GET'])
+def search_resault():
+    """for search"""
+    q = request.args.get('q')
+    categories = Category.query.all()
+    search = Post.title.contains(q) | Post.content.contains(q)
+    posts = Post.query.filter(search).all()
+    if not posts:
+        abort(404)
+    return render_template('news/index.html',
+                           categories=categories,
+                           posts=posts)
+
+@app.errorhandler(404)
+def page404(e):
+    return render_template('news/404.html'), 404
+
+@app.route('/post/create', methods=['POST', 'GET'])
+def create_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        category = request.form['category']
+        category_id = Category.query.filter(Category.title == category).first().id
+
+        post = Post(title=title, content=content, category_id=category_id)
+        db.session.add(post)
+        db.session.commit()
+
+        return redirect(url_for('category_list', id=category_id))
+
+    categories = Category.query.all()
+    form = PostForm()
+    form.category.choices = [cat.title for cat in categories]
+    return render_template('news/create_post.html', form=form)
+
+
+
+# Utils
+
+@app.template_filter('time_filter')
+def jinja2_filter_datatime(date):
+    format = '%d.%m.%Y %H:%M:%S'
+    return date.strftime(format)
 
 if __name__ == '__main__':
     app.run(debug=True)
